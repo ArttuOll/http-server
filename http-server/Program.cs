5dc1e5b618c1bb79074d82d050eebbd7451399a3
ArttuOll <60509537+ArttuOll@users.noreply.github.com>
2024-02-09 10:57:01 +0200

    Create a simple TCP server

---
+++ http-server/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Net;
+using http_server;
+
+var ipAddress = IPAddress.Loopback;
+const int port = 8080;
+
+var server = new Server(ipAddress, port);
+await server.Start();
---
+++ http-server/Server.cs
@@ -0,0 +1,63 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace http_server;
+
+public class Server(IPAddress address, int port)
+{
+    private IPEndPoint IpEndpoint { get; } = new(address, port);
+
+    private readonly TcpListener _listener = new(address, port);
+
+    public async Task Start()
+    {
+        try
+        {
+            _listener.Start();
+            Console.WriteLine($"Listening on {IpEndpoint}");
+
+            while (true)
+            {
+                using var handler = await _listener.AcceptTcpClientAsync();
+                await ServeClient(handler);
+            }
+        }
+        finally
+        {
+            Stop();
+        }
+    }
+
+    private async Task ServeClient(TcpClient client)
+    {
+        await using var stream = client.GetStream();
+        using var streamReader = new StreamReader(stream, Encoding.UTF8);
+        await using var streamWriter = new StreamWriter(stream, Encoding.UTF8);
+
+        while (true)
+        {
+            var received = await streamReader.ReadLineAsync();
+
+            Console.WriteLine($"Received message: {received}.\n", Encoding.UTF8);
+        
+            if (received == "quit")
+            {
+                client.Close();
+                break;
+            }
+        
+            var responseMessage = $"Echo: {received}\n";
+        
+            await streamWriter.WriteAsync(responseMessage);
+            await streamWriter.FlushAsync();
+        
+            Console.WriteLine($"Sent message: {responseMessage}\n");
+        }
+    }
+
+    private void Stop()
+    {
+        _listener.Stop();
+    }
+}
