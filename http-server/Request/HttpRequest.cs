07b760ec9fa0d426cb1c44cf19965dbb629baf55
ArttuOll <60509537+ArttuOll@users.noreply.github.com>
2024-02-13 17:35:22 +0200

    Create HttpRequest and HttpResponse

---
+++ http-server/Request/HttpRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace http_server.Request;
+
+public class HttpRequest(HttpMethod method, Uri uri, string version, Dictionary<string, string> headers)
+{
+    public HttpMethod Method { get; } = method;
+
+    public Uri Uri { get; } = uri;
+
+    public string Version { get; } = version;
+
+    public Dictionary<string, string> Headers { get; } = headers;
+}
---
+++ http-server/Response/HttpResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+
+namespace http_server.Response;
+
+public class HttpResponse
+{
+    public HttpStatusCode StatusCode { get; }
+    
+    public HttpHeaders Headers { get; }
+    
+    public IBodyReader body { get; }
+}
+
+public interface IBodyReader
+{
+    public int Length { get; }
+    
+    public Task<byte[]> Read();
+}
