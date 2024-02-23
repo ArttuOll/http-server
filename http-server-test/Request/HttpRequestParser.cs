2847111b0616ed439fc37c4f63130d615e53b567
ArttuOll <60509537+ArttuOll@users.noreply.github.com>
2024-02-23 17:45:06 +0200

    Add unit tests for HttpRequestParser

---
+++ http-server-test/Request/HttpRequestParser.cs
@@ -0,0 +1,59 @@
+using System.Net;
+using http_server.Request;
+
+namespace http_server_test.Request;
+
+public class HttpRequestParserTests
+{
+    [Fact]
+    public void ParseHeaders()
+    {
+        var headers = new List<string>
+        {
+            "Host: localhost:8080",
+            "User-Agent: Mozilla/5.0"
+        };
+
+        var parsedHeaders = HttpRequestParser.ParseHeaders(headers);
+
+        Assert.Equal("localhost:8080", parsedHeaders["Host"]);
+        Assert.Equal("Mozilla/5.0", parsedHeaders["User-Agent"]);
+    }
+
+    [Fact]
+    public void ParseVersion()
+    {
+        var parsedVersion = HttpRequestParser.ParseVersion("HTTP/1.1");
+
+        Assert.Equal(HttpVersion.Version11.ToString(), parsedVersion);
+    }
+
+    [Fact]
+    public void ParseMethod()
+    {
+        var parsedGet = HttpRequestParser.ParseMethod("GET");
+        var parsedPost = HttpRequestParser.ParseMethod("POST");
+        var parsedPut = HttpRequestParser.ParseMethod("PUT");
+        var parsedDelete = HttpRequestParser.ParseMethod("DELETE");
+
+        Assert.Equal(HttpMethod.Get, parsedGet);
+        Assert.Equal(HttpMethod.Post, parsedPost);
+        Assert.Equal(HttpMethod.Put, parsedPut);
+        Assert.Equal(HttpMethod.Delete, parsedDelete);
+    }
+
+    [Fact]
+    public void ParseHeaderSection()
+    {
+        var headerSection = new List<string>
+        {
+            "GET / HTTP/1.1",
+        };
+
+        var parsedHeaderSection = HttpRequestParser.ParseHeaderSection(headerSection);
+
+        Assert.Equal(HttpMethod.Get, parsedHeaderSection.Method);
+        Assert.Equal(HttpVersion.Version11.ToString(), parsedHeaderSection.Version);
+        Assert.Equal("/", parsedHeaderSection.Uri);
+    }
+}
--- http-server/Request/HttpRequestParser.cs
+++ http-server/Request/HttpRequestParser.cs
@@ -8,24 +8,27 @@ public static class HttpRequestParser
     {
         try
         {
+            // The request line is of the form: method SP request-target SP HTTP-version
             var requestLine = headerSection[0].Split(' ');
+
             var method = ParseMethod(requestLine[0]);
             var requestTarget = requestLine[1];
-            var version = ParseVersion(requestLine[2]);
+            var httpVersion = ParseVersion(requestLine[2]);
+
             var headers = ParseHeaders(
                 headerSection.GetRange(1, headerSection.Count - 1)
             );
 
-            return new HttpRequest(method, uri, version, headers);
+            return new HttpRequest(method, requestTarget, httpVersion, headers);
         }
-        catch (Exception)
+        catch (Exception innerException)
         {
-            throw new HttpRequestException(400, "Error parsing the header section of the request.");
+            throw new HttpRequestException(400, "Error parsing the header section of the request.", innerException);
         }
 
     }
 
-    private static HttpMethod ParseMethod(string method)
+    public static HttpMethod ParseMethod(string method)
     {
         return new HttpMethod(method);
     }
@@ -42,7 +45,7 @@ public static class HttpRequestParser
         };
     }
 
-    private static Dictionary<string, string> ParseHeaders(IEnumerable<string> headers)
+    public static Dictionary<string, string> ParseHeaders(IEnumerable<string> headers)
     {
         return headers.Select(header => header.Split(": ")).ToDictionary(headerParts => headerParts[0], headerParts => headerParts[1]);
     }
