e0db8428613396e9a2894893c39acdb952f06745
ArttuOll <60509537+ArttuOll@users.noreply.github.com>
2024-02-23 17:44:54 +0200

    Add inner exception to HttpRequestException

--- http-server/Request/HttpRequestException.cs
+++ http-server/Request/HttpRequestException.cs
@@ -1,6 +1,6 @@
 ﻿namespace http_server.Request;
 
-public class HttpRequestException(int statusCode, string message) : Exception(message)
+public class HttpRequestException(int statusCode, string message, Exception? innerException = null) : Exception(message, innerException)
 {
     public int StatusCode { get; } = statusCode;
 }
