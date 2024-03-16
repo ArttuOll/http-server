aea03da18199668a78d6ba3a350b6ccbb25dfbea
ArttuOll <60509537+ArttuOll@users.noreply.github.com>
2024-03-16 16:50:30 +0200

    Create HttpRequestHandler

---
+++ http-server/Request/HttpRequestHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using http_server.Body;
+using http_server.Response;
+
+namespace http_server.Request;
+
+public class HttpRequestHandler(HttpRequest request, IBodyReader body)
+{
+    public HttpResponse Handle()
+    {
+        var headers = new Dictionary<string, string>
+        {
+            { "Content-Type", "text/plain" },
+            { "Server", "http-server" }
+        };
+
+        return request.Uri switch
+        {
+            "/echo" => new HttpResponse(HttpStatusCode.OK, headers, body),
+            _ => new HttpResponse(HttpStatusCode.OK, headers, null)
+        };
+    }
+}
